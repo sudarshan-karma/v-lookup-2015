59f500017cc626edcefa10919785f02fd7be7b1b
Sudarshan Bhattacharjee <sudarshan.karma@gmail.com>
2015-08-05 16:24:51 -0700

    Setting up Express with Jade template
    Signed-off-by: Sudarshan Bhattacharjee <sudarshan.karma@gmail.com>

--- app.js
+++ app.js
@@ -1,35 +1,44 @@
-
-/**
- * Module dependencies.
- */
-
 var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+	, routes = require('./routes')
+	, user = require('./routes/user');
 
 var app = express();
 
-// all environments
-app.set('port', process.env.PORT || 3000);
+app.use(app.router);
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
-
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
 
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+
+/*
+
+ app.get('/cool', function(request, response) {
+  response.send(cool());
 });
+
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+})
+*/
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
